994a899a43e1ee636cdd9da39ceb8be03fc61eaa
Andrii Zakharov <Andrii.Zakharov@gettyimages.com>
2016-10-28 17:18:24 +0300

    deleted unused import

--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -2,7 +2,6 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
