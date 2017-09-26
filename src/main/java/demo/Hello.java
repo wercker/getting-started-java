241d5708d0f9a11480049640967c58424296df72
Rik Gibson <rikgibson@gmail.com>
2017-09-26 11:51:25 +0100

    Update yaml and readme to match current CLI (#3)
    * Update yaml and readme to match current CLI
    
    * minor changes per benno

--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,8 +1,7 @@
-package com.example;
+package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +11,7 @@ public class Hello {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello World";
+		return "Hello World!";
 	}
 
 
